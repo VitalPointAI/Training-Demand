f75b517e5d550bee0e18e6f3482d19101918c3c5
ALuhning <a.luhning@vitalpoint.ai>
2019-09-15 19:07:35 -0400

    fix to enable loading

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.css'
+
 import App from './components/App'
 import configureStore from './store/configureStore'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+      <App />
+  </Provider>
+  ,document.querySelector('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
