564c4b5b7849153d4f1e4b25ff14fe77fda8ba67
VitalPointAI <a.luhning@vitalpoint.ai>
2019-09-22 06:18:48 -0400

    position creation

--- src/contracts/Demand.sol
+++ src/contracts/Demand.sol
@@ -1,9 +1,12 @@
 pragma solidity ^0.5.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Mintable.sol";
 import "@openzeppelin/contracts/access/Roles.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract Demand {
+contract Demand is ERC721Full, ERC721Mintable, Ownable {
 
     using Roles for Roles.Role;
     using SafeMath for uint256;
@@ -16,11 +19,105 @@ contract Demand {
     Roles.Role private _soldier;
     Roles.Role private _trainingEstablishment;
 
+    enum Rank { PteB, AvrB, OS, PteT, AvrT, AB, Cpl, LS, MCpl, MS,
+                Sgt, PO2, WO, PO1, MWO, CPO2, CWO, CPO1, OCdt, NCdt,
+                ASlt, SecLt, Lt, SLt, Capt, LtN, Maj, LCdr, LCol, Cdr,
+                Col, CaptN, BGen, Cmdre, MGen, RAdm, LGen, Gen, Adm }
+
+    enum Trade { ACOPAEROOP, ACCISS, ACSTECH, ACSO, AEC, AERE, AESOP,
+                AMSUP, AMMOTECH, ARMD, ARTY, ARTYMNAD, ARTYMNFD, ATISTECH,
+                AVPHYSTECH, AVNTECH, AVSTECH, AWSTECH, BETECH, BIO, BOSN,
+                CBRNOP, CBTENGR, CESUPT, CELEAIR, CHAP, CIC, CLDVR, COMMRSCH,
+                CONSTENGR, CONSTTECH, COOK, CR, CRMN, CRTRPTR, CYBEROP, DENT,
+                DENTTECH, DSTECH, ETECH, EDTECH, EGSTECH, EME, ENGR, EOTECHL,
+                FIREFTR, FLTENGR, FSA, GEOTECH, GNR, GOL, GOSCHAPP, HTECH, HCA,
+                HRA, HSO, IMAGETECH, INF, INFMN, INT, INTOP, LCISTECH, LEGAL,
+                LMN, LOG, MAREL, MARENG, MARENGART, MARTECH, MARSSS, MATTECH, MED,
+                MEDTECH, MESO, METTECH, MLABTECH, MP, MPO, MRADTECH, MSENG, MSEOP,
+                MUSC, MUSCN, NAVCOMM, NAVENG, NCIOP, NCSENG, NDTTECH, NESOP, NUR, ORTECH,
+                PAO, PANDD, PHTECH, PHARM, PHYSIO, PID, PLT, PMEDTECH, POSTCLK,
+                PSEL, RMTECH, RMSCLK, SARTECH, SFOP, SIGOP, SIGS, SOCW, SONAROP,
+                STWD, SUPTECH, TFCTECH, TRGDEV, VEHTECH, WENGTECH, WTECHL, WFETECH }
+
+    enum Status { active, inactive, obsolete }
+    enum Component { RegF, ResF, Civ }
+    enum Environment { CA, RCN, RCAF, CANSOFCOM }
+
+    struct Position {
+        uint256 positionId;
+        string positionNumber;
+        string positionName;
+        Rank lowRank;
+        Rank highRank;
+        Trade trade;
+        Environment environment;
+        uint256 taskList;
+        uint256 currentSoldier;
+        address owningOrganization;
+        Status status;
+        Component component;
+    }
+
+    struct Soldier {
+        string serviceNumber;
+        string[] currentQualifications;
+        string[] desiredQualifications;
+        Rank rank;
+        Trade trade;
+        Environment environment;
+        Component component;
+        address soldierAddress;
+    }
+
+    struct Task {
+        uint256 taskId;
+    }
+
+    struct Qualification {
+        uint256 qualificationId;
+        string competencyCode;
+        string qualificationTitle;
+    }
+
+    struct Organization {
+        uint256 organizationId;
+        string organizationName;
+        address organizationAddress;
+    }
+
+    Position[] public positions;
+    Soldier[] public soldiers;
+    Task[] public tasks;
+    Qualification[] public qualifications;
+
+    // Events
+    event PositionCreated(
+        uint256 indexed positionId,
+        string positionNumber,
+        string positionName,
+        Rank lowRank,
+        Rank highRank,
+        Trade trade,
+        Environment environment,
+        uint256 taskList,
+        uint256 currentSoldier,
+        address indexed owningOrganization,
+        Status status,
+        Component component
+    );
+
+    constructor(string memory _identifier, string memory _symbol) ERC721Full(_identifier, _symbol) public onlyOwner {
+    }
+
+    function terminateDemand() public onlyOwner {
+        selfdestruct(msg.sender);
+    }
+
     // Create contract and assign administrator access
-    constructor(address[] memory administrators) public {
-        for (uint256 i = 0; i < administrators.length; ++i) {
-            _administrator.add(administrators[i]);
-        }
+    function assignAdministratorRole(address[] memory administrators) public onlyOwner {
+        for (uint256 i = 0; i < administrators.length; i++) {
+                _administrator.add(administrators[i]);
+            }
     }
 
     // Role Assignments an Administrator must make
@@ -52,6 +149,101 @@ contract Demand {
         }
     }
 
+    // Role Assignments a Recruiter (or Administrator) must make
+    function assignSoldierRole(address[] memory soldiersAddress) public {
+        require((_administrator.has(msg.sender) ||
+                _recruiter.has(msg.sender)),
+                "DOES_NOT_HAVE_ADMINISTRATOR_OR_RECRUITER_ROLE");
+        for (uint256 i = 0; i < soldiersAddress.length; ++i) {
+            _soldier.add(soldiersAddress[i]);
+        }
+    }
 
+    // Role Assignments an Establishment Manager (or Administrator) must make
+    function assignUnitPositionManagerRole(address[] memory unitPositionManagers) public {
+        require((_administrator.has(msg.sender) ||
+                _establishmentManager.has(msg.sender)),
+                "DOES_NOT_HAVE_ADMINISTRATOR_OR_ESTABLISHMENT_MANAGER_ROLE");
+        for (uint256 i = 0; i < unitPositionManagers.length; ++i) {
+            _unitPositionManager.add(unitPositionManagers[i]);
+        }
+    }
 
+    // Creation Functions
+    function createPosition(string memory _positionNumber,
+                            string memory _positionName,
+                            Rank _lowRank,
+                            Rank _highRank,
+                            Trade _trade,
+                            Environment _environment,
+                            uint256 _taskList,
+                            uint256 _currentSoldier,
+                            address _owningOrganization,
+                            Status _status,
+                            Component _component
+                            ) public {
+        require((_administrator.has(msg.sender) ||
+            _establishmentManager.has(msg.sender)),
+            "DOES_NOT_HAVE_ADMINISTRATOR_OR_ESTABLISHMENT_MANAGER_ROLE");
+        uint256 positionId = positions.length; // generates unique positionId
+        positions.push(Position(
+                                positionId,
+                                _positionNumber,
+                                _positionName,
+                                _lowRank,
+                                _highRank,
+                                _trade,
+                                _environment,
+                                _taskList,
+                                _currentSoldier,
+                                _owningOrganization,
+                                _status,
+                                _component
+                                ));
+        _mint(_owningOrganization, positionId); // Create a new position
+        emit PositionCreated(
+                            positionId,
+                            _positionNumber,
+                            _positionName,
+                            _lowRank,
+                            _highRank,
+                            _trade,
+                            _environment,
+                            _taskList,
+                            _currentSoldier,
+                            _owningOrganization,
+                            _status,
+                            _component
+        );
+    }
+
+    function getPositionDetails(uint _positionId)
+        public view returns(
+            uint256 positionId,
+            string memory positionNumber,
+            string memory positionName,
+            Rank lowRank,
+            Rank highRank,
+            Trade trade,
+            Environment environment,
+            uint256 taskList,
+            uint256 currentSoldier,
+            address owningOrganization,
+            Status status,
+            Component component
+        ) {
+        Position memory _positions = positions[_positionId];
+        positionId = _positions.positionId;
+        positionNumber = _positions.positionNumber;
+        positionName = _positions.positionName;
+        lowRank = _positions.lowRank;
+        highRank = _positions.highRank;
+        trade = _positions.trade;
+        environment = _positions.environment;
+        taskList = _positions.taskList;
+        currentSoldier = _positions.currentSoldier;
+        owningOrganization = _positions.owningOrganization;
+        status = _positions.status;
+        component = _positions.component;
+    }
 }
--- test/Demand.js
+++ test/Demand.js
@@ -4,18 +4,233 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Demand', ([administrator]) => {
-    let instance
+const BN = require('bn.js')
+require('chai')
+    .use(require('chai-bn')(BN))
+
+contract('Demand', ([contractOwner, administrator1, administrator2, soldier1, unitPositionManager1, careerManager1, recruiter1, trainingEstablishment1 ]) => {
+
+    let demandContract
+    let administrators
+    let soldiers
+    let unitPositionManagers
+    let establishmentManagers
+    let careerManagers
+    let recruiters
+    let trainingEstablishments
+
+    let positionId
+    let positionNumber = '123456'
+    let positionName = 'OpsO'
+    let lowRank = 24
+    let highRank = 26
+    let trade = 56
+    let environment = 0
+    let currentSoldier = 100
+    let taskList = 0
+    let status = 0
+    let owningOrganization = contractOwner
+    let component = 0
 
     beforeEach(async () => {
-        // Fetch Demand contract from blockchain
-        instance = await Demand.deployed()
-    })
 
-    describe('deployment', () => {
-        instance('should assign first account as an administrator', async() => {
-            let deployer = await instance._administrator(0)
-            deployer.should.equal(administrator)
-        })
-    })
+        demandContract = await Demand.new("CAFChain", "CAF")
+
+        try {
+            await demandContract.assignAdministratorRole([administrator1, administrator2], { from: contractOwner })
+            await demandContract.assignSoldierRole([soldier1], { from: administrator1 })
+            await demandContract.assignUnitPositionManagerRole([unitPositionManager1], { from: administrator1 })
+            await demandContract.assignCareerManagerRole([careerManager1], { from: administrator1 })
+            await demandContract.assignRecruiterRole([recruiter1], { from: administrator1 })
+            await demandContract.assignTrainingEstablishmentRole([trainingEstablishment1], { from: administrator1 })
+        } catch (e) {
+            console.log("Error " + e)
+        }
+    });
+
+    describe('role assignment', () => {
+        it('ensure owner is the first address', async () => {
+            const owner = await demandContract.owner()
+            owner.should.equal(contractOwner)
+        });
+        it('ensure administrator1 has been given administrator rights', async () => {
+            try {
+                await demandContract.createPosition(
+                    positionNumber,
+                    positionName,
+                    lowRank,
+                    highRank,
+                    trade,
+                    environment,
+                    taskList,
+                    currentSoldier,
+                    owningOrganization,
+                    status,
+                    component,
+                    { from: administrator1 }
+                    );
+            } catch (e) {
+                console.log(`Error: ${e}`);
+            }
+            let details = await demandContract.getPositionDetails(0)
+            details.positionName.should.to.deep.equal("OpsO")
+        });
+        it('ensure administrator2 has been given administrator rights', async () => {
+            try {
+                await demandContract.createPosition(
+                    positionNumber,
+                    positionName,
+                    lowRank,
+                    highRank,
+                    trade,
+                    environment,
+                    taskList,
+                    currentSoldier,
+                    owningOrganization,
+                    status,
+                    component,
+                    { from: administrator2 }
+                    );
+            } catch (e) {
+                console.log(`Error: ${e}`);
+            }
+            let details = await demandContract.getPositionDetails(0)
+            details.positionName.should.to.deep.equal("OpsO")
+        });
+        it('ensure soldier1 does not have administrator rights', async () => {
+            try {
+                await demandContract.createPosition(
+                    positionNumber,
+                    positionName,
+                    lowRank,
+                    highRank,
+                    trade,
+                    environment,
+                    taskList,
+                    currentSoldier,
+                    owningOrganization,
+                    status,
+                    component,
+                    { from: soldier1 }
+                    );
+            } catch (e) {
+                e.should.to.deep.equal(e);
+            }
+        });
+        it('ensure unitPositionManager1 does not have administrator rights', async () => {
+            try {
+                await demandContract.createPosition(
+                    positionNumber,
+                    positionName,
+                    lowRank,
+                    highRank,
+                    trade,
+                    environment,
+                    taskList,
+                    currentSoldier,
+                    owningOrganization,
+                    status,
+                    component,
+                    { from: unitPositionManager1 }
+                    );
+            } catch (e) {
+                e.should.to.deep.equal(e);
+            }
+        });
+        it('ensure careerManager1 does not have administrator rights', async () => {
+            try {
+                await demandContract.createPosition(
+                    positionNumber,
+                    positionName,
+                    lowRank,
+                    highRank,
+                    trade,
+                    environment,
+                    taskList,
+                    currentSoldier,
+                    owningOrganization,
+                    status,
+                    component,
+                    { from: careerManager1 }
+                    );
+            } catch (e) {
+                e.should.to.deep.equal(e);
+            }
+        });
+        it('ensure recruiter1 does not have administrator rights', async () => {
+            try {
+                await demandContract.createPosition(
+                    positionNumber,
+                    positionName,
+                    lowRank,
+                    highRank,
+                    trade,
+                    environment,
+                    taskList,
+                    currentSoldier,
+                    owningOrganization,
+                    status,
+                    component,
+                    { from: recruiter1 }
+                    );
+            } catch (e) {
+                e.should.to.deep.equal(e);
+            }
+        });
+        it('ensure trainingEstablishment1 does not have administrator rights', async () => {
+            try {
+                await demandContract.createPosition(
+                    positionNumber,
+                    positionName,
+                    lowRank,
+                    highRank,
+                    trade,
+                    environment,
+                    taskList,
+                    currentSoldier,
+                    owningOrganization,
+                    status,
+                    component,
+                    { from: trainingEstablishment1 }
+                    );
+            } catch (e) {
+                e.should.to.deep.equal(e);
+            }
+        });
+    });
+    describe('Position Creation', () => {
+        it('administrator should properly create a position', async () => {
+            try {
+                await demandContract.createPosition(
+                    positionNumber,
+                    positionName,
+                    lowRank,
+                    highRank,
+                    trade,
+                    environment,
+                    taskList,
+                    currentSoldier,
+                    owningOrganization,
+                    status,
+                    component,
+                    { from: administrator1 }
+                    );
+            } catch (e) {
+                console.log(`Error: ${e}`);
+            }
+            let details = await demandContract.getPositionDetails(0)
+            assert.equal(details.positionId, 0, "positionId is not correct")
+            assert.equal(details.positionNumber, '123456', "positionNumber is not correct")
+            assert.equal(details.positionName, 'OpsO', "positionName is not correct")
+            assert.equal(details.lowRank, 24, "lowRank is not correct")
+            assert.equal(details.highRank, 26, "highRank is not correct")
+            assert.equal(details.trade, 56, "trade is not correct")
+            assert.equal(details.environment, 0, "environment is not correct")
+            assert.equal(details.taskList, 0, "taskList is not correct")
+            assert.equal(details.currentSoldier.toString(), currentSoldier.toString(), "currentSoldier is not correct")
+            assert.equal(details.owningOrganization, contractOwner, "owningOrganization is not correct")
+            assert.equal(details.status, 0, "status is not correct")
+            assert.equal(details.component, 0, "component is not correct")
+        });
+    });
 })
